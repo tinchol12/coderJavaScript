e8de954972c19f8a654f93e79fd937621aee422b
Tincho <martin.osses@gmail.com>
2022-05-19 09:20:26 -0300

    Entregable 02

---
+++ Proyecto Entregable/fasttexchange/Readme.txt
@@ -0,0 +1,22 @@
+Crear un sitio web simulando un exchange de criptos. El usuario debe ser capaz de:
+
+1- Registrarse
+2. Logearse
+3. Cargar saldo
+4. Consultar cotizaciones en tiempo real (APIS)
+5. Realizar operaciones desde el saldo en ARS con otras monedas
+6. Invertir en diferentes FCI
+
+7. Consultar Cartera de inversiones
+8. Retirar saldo
+9. Estimar ganancias en FCI durante un período de tiempo
+10. Automatización de pruebas con Cypress.io
+    - Login
+    - Registrarse
+    - Consular estado de las APIS
+    - Generar una operacion Pesos = $Coin
+    - Invertir en un FCI
+    - Calcular rendimientos durante un período de tiempo
+    - Retirar Saldo
+    - Reporte de Casos ejecutados
+
---
+++ Proyecto Entregable/fasttexchange/js_ejercicios/entregable.js
@@ -0,0 +1,145 @@
+
+
+/* Variables globales  */
+
+const hardPassword = "1234";
+let saldoDisponible = 1000;
+const ualaFci = 0.3825;
+const ualaMeli = 0.3945;
+const ualaBalanz = 0.44;
+let formulaRendimiento = 0;
+let opcionPrincipal = 0
+let counter = 5;
+
+/* Run  */ 
+
+const nombreUsuario = prompt("Usuario: ");
+let password = prompt("Password: ");
+
+do  {
+    if (password == hardPassword) {
+        alert('Bienvenido ' + nombreUsuario);
+        openMenuPrincipal();
+        break;        
+    } else {
+        alert('Usuario o contraseña incorrectos. Recuerde que tiene ' + counter + ' intentos disponibles');
+        password = prompt("Password: ");
+        counter--;
+    }
+} while (counter != 0); 
+if(counter == 0) {
+    alert('Usted ha agotado los intentos disponibles. Por favor, intente mas tarde');
+}
+
+
+/* Funciones  */
+
+
+
+function openMenuPrincipal() {
+    
+    opcionPrincipal = prompt("Ingrese la opción que desea realizar: \n 1. Consulta Saldo \n 2. Operaciones \n 3. Contactar a Soporte \n 4. Salir");
+
+    switch (opcionPrincipal) {
+
+        case '1':
+            consultaSaldo(saldoDisponible);
+            break;
+        case '2':
+            openMenuOperaciones();
+            break;
+        case '3':
+            contacto();
+            break;
+        case '4':
+            salir();
+            break;
+        default:
+            alert("Ha ingresado una opción inválida");            
+            break;
+    }    
+}
+
+function openMenuOperaciones() {
+    
+    restartOperationsFci();
+
+    switch (opcionSeleccion) {
+
+        case '1':
+            invertirFCI(saldoDisponible);
+            break;
+        case '2':
+            comprarCripto(saldoDisponible);
+            break;
+        case '3':
+            openMenuPrincipal();
+            break;
+        default:
+            alert("Ha ingresado una opción inválida"); 
+            openMenuPrincipal();           
+            break;
+    }    
+}
+
+function restartOperationsFci(){
+    opcionSeleccion = prompt("Ingrese la opción que desea realizar: \n 1. Invertir en FCI \n 2. Comprar de Cripto \n 3. Salir");
+}
+
+function invertirFCI(saldoDisponible) {
+    let seleccionarFondo = +prompt("Seleccione el fondo en el cual va a invertir: \n 1. UALA (38.25 %) \n 2. MELI (39.45 %) \n 3. BALANZ (44 %) \n 4. VER TODOS LOS RENDIMIENTOS \n 5. Volver atras");
+    const CantidadDias = prompt("Ingrese la cantidad de dias que desea invertir: ");
+    let rendimiento = 0;
+      
+    if(seleccionarFondo === 1){
+        formulaRendimiento = (ualaFci / 365) + 1;
+        rendimiento = Number(CantidadDias) * ((Number(formulaRendimiento)) * Number(saldoDisponible) - Number(saldoDisponible));
+        alert("Obtiene: $" + rendimiento.toFixed(2) + " invirtiendo $" + saldoDisponible + " durante " + CantidadDias + " dias.");
+        console.log(`seleccionarFondo ${seleccionarFondo}, formulaRendimiento ${formulaRendimiento}`);
+    }
+    else if(seleccionarFondo === 2){
+        formulaRendimiento = (ualaMeli / 365) + 1;
+        rendimiento = Number(CantidadDias) * ((Number(formulaRendimiento)) * Number(saldoDisponible) - Number(saldoDisponible));
+        alert("Obtiene: $" + rendimiento.toFixed(2) + " invirtiendo $" + saldoDisponible + " durante " + CantidadDias + " dias.");
+        console.log(`seleccionarFondo ${seleccionarFondo}, formulaRendimiento ${formulaRendimiento}`);
+    }
+    else if(seleccionarFondo === 3){
+        formulaRendimiento = (ualaBalanz / 365) + 1;
+        rendimiento = Number(CantidadDias) * ((Number(formulaRendimiento)) * Number(saldoDisponible) - Number(saldoDisponible));
+        alert("Obtiene: $" + rendimiento.toFixed(2) + " invirtiendo $" + saldoDisponible + " durante " + CantidadDias + " dias.");
+        console.log(`seleccionarFondo ${seleccionarFondo}, formulaRendimiento ${formulaRendimiento}`);
+    }
+    else if(seleccionarFondo === 4){
+        formulaRendimiento = (ualaFci / 365) + 1;
+        let rendimientoUala = Number(CantidadDias) * ((Number(formulaRendimiento)) * Number(saldoDisponible) - Number(saldoDisponible));
+        formulaRendimiento = (ualaMeli / 365) + 1;
+        let rendimientoMeli = Number(CantidadDias) * ((Number(formulaRendimiento)) * Number(saldoDisponible) - Number(saldoDisponible));
+        formulaRendimiento = (ualaBalanz / 365) + 1;
+        let rendimientoBalanz = Number(CantidadDias) * ((Number(formulaRendimiento)) * Number(saldoDisponible) - Number(saldoDisponible));
+
+        alert("Rendimientos para una inversión de: $" + saldoDisponible + " durante " + CantidadDias + " dias: \n 1. UALA $"+ rendimientoUala.toFixed(2) + " \n 2. MELI $"+ rendimientoMeli.toFixed(2) + " \n 3. BALANZ $"+ rendimientoBalanz.toFixed(2) + "");
+    }
+    else if(seleccionarFondo === 5){
+        openMenuOperaciones();
+    }
+    
+    opcionSeleccion = 0;
+    openMenuOperaciones();
+
+    
+}
+
+function consultaSaldo(saldoDisponible) {
+    alert("Su saldo disponible es de: " + saldoDisponible);
+    openMenuPrincipal();
+}
+
+function salir() {
+    alert("Gracias por utilizar nuestros servicios");
+}
+
+function comprarCripto(saldoDisponible) {
+    const cantidadComprar = prompt("Ingrese la cantidad que desea comprar: ");
+    const valorComprar = Number(cantidadComprar) * Number(saldoDisponible);
+    alert("La cantidad a comprar es de: " + valorComprar);
+}
