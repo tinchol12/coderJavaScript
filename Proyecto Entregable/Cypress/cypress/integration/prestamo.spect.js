5099167fdac649878cedc7ce4878c93b182b4d19
Tincho <martin.osses@gmail.com>
2022-06-29 08:34:00 -0300

    up

--- Proyecto Entregable/Cypress/cypress/integration/prestamo.spect.js
+++ Proyecto Entregable/Cypress/cypress/integration/prestamo.spect.js
@@ -7,7 +7,7 @@ describe('Pruebas sobre el simulador de Préstamos', () => {
 
     it('Desde la barra de navegación ir a la seccion Criptos', () => {
         cy.get('[href="#section-1"]').click();
-        cy.get('[data-cy="36"]').should('have.text', 'Préstamos');
+        cy.get('[cy-data="36"]').should('have.text', 'Préstamos');
     })
 
     it('Validar que el título diga Registrate', () => {
@@ -32,7 +32,7 @@ describe('Pruebas sobre el simulador de Préstamos', () => {
         cy.get('[id="tiempo"').should('have.text', '');
     });  
 
-    it.only('No puede ingresar un valor mayor a 8 en cuotas', () => {
+    it('No puede ingresar un valor mayor a 8 en cuotas', () => {
         cy.get('[id="tiempo"').type('9');
         cy.get('[id="btnCalcular"]').click();
         cy.get('[id="swal2-html-container"]').should('have.text', 'No otorgamos préstamos mayores a 8 meses, ni prestamos en $0');
@@ -59,30 +59,7 @@ describe('Pruebas sobre el simulador de Préstamos', () => {
         cy.get('[id="lista-tabla"]').should('be.visible');
     }); 
     
-    it('Cotiza USD --> Bitcoin', () => {
 
-        cy.get('[id="moneda"]').select('USD');
-        cy.get('[id="criptomonedas"]').select('Bitcoin');
-        cy.get('[value="Cotizar"]').click();
-        cy.get('[id="swal2-html-container"]').should('have.text', 'Cotización de Crpto en el acto exitosa');
-        cy.get('[class="swal2-confirm swal2-styled"]').click();
-        cy.get('[class="main-price"]').should('be.visible');
-    });
-
-    it('Seleccion entre USD --> Bitcoint y luego Etherium', () => {
-
-        cy.get('[id="moneda"]').select('USD');
-        cy.get('[id="criptomonedas"]').select('Bitcoin');
-        cy.get('[value="Cotizar"]').click();
-        cy.get('[id="swal2-html-container"]').should('have.text', 'Cotización de Crpto en el acto exitosa');
-        cy.get('[class="swal2-confirm swal2-styled"]').click();
-        cy.get('[class="main-price"]').should('be.visible');
-        cy.get('[id="criptomonedas"]').select('Ethereum');
-        cy.get('[value="Cotizar"]').click();
-        cy.get('[id="swal2-html-container"]').should('have.text', 'Cotización de Crpto en el acto exitosa');
-        cy.get('[class="swal2-confirm swal2-styled"]').click();
-        cy.get('[class="main-price"]').should('be.visible');
-    });
 
    
 
